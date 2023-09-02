60df4d73d3754dc83862dab9060734d312091064
bruno <amorimbq@gmail.com>
2023-09-02 20:30:28 -0300

    refactor: separated the app from the server start for better tests

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,5 @@
-import fastify from 'fastify'
+import { app } from './app'
 import { knex } from './database'
-import { root } from './routes/root'
-import cookie from '@fastify/cookie'
-
-const app = fastify()
-
-app.register(cookie)
-app.addHook('onSend', async (request, reply) =>
-  console.log(`[${request.method}] ${request.url} - [${reply.statusCode}]`),
-)
-app.register(root, { prefix: 'api' })
 
 app.listen({ port: 3000 }, async () => {
   try {
