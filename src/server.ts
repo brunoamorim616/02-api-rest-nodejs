b3803e7a3e1e94dcb03057822f89e0201890f407
bruno <amorimbq@gmail.com>
2023-09-02 22:18:29 -0300

    fix: removed sqlite query on server start

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,8 @@
 import { app } from './app'
-import { knex } from './database'
 import { env } from './env'
 
 app.listen({ port: env.PORT }, async () => {
   try {
-    if (env.DATABASE_CLIENT === 'sqlite')
-      await knex('sqlite_schema').select(`*`)
     console.log('Server is UP!')
   } catch (error) {
     console.error(error)
