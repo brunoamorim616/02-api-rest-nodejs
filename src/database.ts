c2ceaf1f967116490eb9db9d1ffd89dd522845a4
bruno <amorimbq@gmail.com>
2023-09-02 22:02:20 -0300

    fix: database client dynamically set

--- src/database.ts
+++ src/database.ts
@@ -1,17 +1,15 @@
 import { Knex, knex as setupKnex } from 'knex'
 import { env } from './env'
 
-const connection =
-  env.DATABASE_CLIENT === 'sqlite'
-    ? {
-        filename: env.DATABASE_URL,
-        port: env.PORT,
-      }
-    : env.DATABASE_URL
-
 export const config: Knex.Config = {
-  client: 'sqlite3',
-  connection,
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_URL,
+          port: env.PORT,
+        }
+      : env.DATABASE_URL,
   migrations: {
     extension: 'ts',
     directory: './db/migrations',
