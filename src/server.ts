d525f6a2b36db046f14c4910272cce376ce3bc07
bruno <amorimbq@gmail.com>
2023-09-02 22:14:24 -0300

    fix: execute sqlite health check when client is sqlite

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,8 @@ import { env } from './env'
 
 app.listen({ port: env.PORT }, async () => {
   try {
-    await knex('sqlite_schema').select(`*`)
+    if (env.DATABASE_CLIENT === 'sqlite')
+      await knex('sqlite_schema').select(`*`)
     console.log('Server is UP!')
   } catch (error) {
     console.error(error)
